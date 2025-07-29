816e63813095b80df593b51dcbaccb84bc21bd68
lehuy0311 <lehuy0311@gmail.com>
2025-07-29 17:04:52 +0700

    Fix lỗi build config

--- config.py
+++ config.py
@@ -3,10 +3,20 @@
 import os
 
 # --- Cấu hình Neo4j AuraDB Kết nối Neo4j với đồ thị---
-NEO4J_URI = "neo4j+s://1a362d71.databases.neo4j.io"
-NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "g92O8cbIFTgQqYRe8MckNUJ9fcI8GOpPIfJflZLQNN4"
 
-GEMINI_API_KEY = "AIzaSyCPHsDMa90aQMvp5KpyQF_id6pG9z9GB2g" 
-MODEL_LLM = "models/gemini-1.5-flash"  #  models/gemini-pro
+# NEO4J_URI = "neo4j+s://1a362d71.databases.neo4j.io"
+# NEO4J_USERNAME = "neo4j"
+# NEO4J_PASSWORD = "g92O8cbIFTgQqYRe8MckNUJ9fcI8GOpPIfJflZLQNN4"
+
+# GEMINI_API_KEY = "AIzaSyCPHsDMa90aQMvp5KpyQF_id6pG9z9GB2g" 
+# MODEL_LLM = "models/gemini-1.5-flash"  #  models/gemini-pro
+# MODEL_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MODEL_LLM = "models/gemini-1.5-flash"
 MODEL_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
